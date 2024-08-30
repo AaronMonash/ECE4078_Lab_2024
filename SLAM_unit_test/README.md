0f867d537b5e8696bafba2409e42a330ac6e1991
Binh Tran <Binh.Tran@monash.edu>
2024-08-30 11:22:51 +1000

    SLAM unit test update

--- SLAM_unit_test/README.md
+++ SLAM_unit_test/README.md
@@ -2,21 +2,30 @@
 Credit Tin Tran
 
 ## Usage
-This unit test is used to evaluate your `robot.py` and `EKF.py`. Successfully passing the unit test will give you a good indication that you have implemented SLAM correctly, and may procede to the next tasks. Otherwise, the test report will let you know which part you should be looking into for a correct SLAM implementation.
+* This unit test is used to evaluate your `robot.py` and `EKF.py`. 
+
+* Successfully passing the unit test will give you a good indication that you have implemented SLAM correctly, and may procede to the next tasks. 
+
+* Otherwise, the test report will let you know which part you of your SLAM might be, but not necessarily, incorrect.
 
 ## Preparation
 1. Copy from your `Week02-04` folder into this folder, `SLAM_unit_test`, the following files and folders:
-    * `operate.py`
     * `calibration`
-    * `slam`
     * `util`
     * `pics`
 
-2. Replace your baseline and scale with the provided [baseline.txt](./baseline.txt) and [scale.txt](./scale.txt).
+2. Replace the following [TODOs] in the skeleton codes with your own implementations:
+    * In [operate.py](./operate.py), [line 200](./operate.py#L200) to [line 213](./operate.py#L213).
+    * In [robot.py](./slam/robot.py), [line 79](./slam/robot.py#79) and [line 127](./slam/robot.py#L127)
+    * In [ekf.py](./slam/ekf.py), [line 93](./slam/ekf.py#L93) and [line 117](./slam/ekf.py#L117)
 
-3. Activate your virtual environment.
+    [NOTE] It is important that you keep everything else in the skeleton codes the same!
 
-4. Install package `pytest` using
+3. Replace your baseline and scale with the provided [baseline.txt](./baseline.txt) and [scale.txt](./scale.txt).
+
+4. Activate your virtual environment.
+
+5. Install package `pytest` using
 ```
 python -m pip install pytest
 ```
---
+++ SLAM_unit_test/slam/aruco_detector_old.py
@@ -0,0 +1,51 @@
+# detect ARUCO markers and estimate their positions
+import numpy as np
+import cv2
+import os, sys
+
+sys.path.insert(0, "{}/util".format(os.getcwd()))
+import util.measure as measure
+
+class aruco_detector:
+    def __init__(self, robot, marker_length=0.07):
+        self.camera_matrix = robot.camera_matrix
+        self.distortion_params = robot.camera_dist
+
+        self.marker_length = marker_length
+        self.aruco_params = cv2.aruco.DetectorParameters_create()
+        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
+    
+    def detect_marker_positions(self, img):
+        # Perform detection
+        corners, ids, rejected = cv2.aruco.detectMarkers(
+            img, self.aruco_dict, parameters=self.aruco_params)
+        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+            corners, self.marker_length, self.camera_matrix, self.distortion_params)
+        # rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(corners, self.marker_length, self.camera_matrix, self.distortion_params) # use this instead if you got a value error
+
+        if ids is None:
+            return [], img
+
+        # Compute the marker positions
+        measurements = []
+        seen_ids = []
+        for i in range(len(ids)):
+            idi = ids[i,0]
+            # Some markers appear multiple times but should only be handled once.
+            if idi in seen_ids:
+                continue
+            else:
+                seen_ids.append(idi)
+
+            lm_tvecs = tvecs[ids==idi].T
+            lm_bff2d = np.block([[lm_tvecs[2,:]],[-lm_tvecs[0,:]]])
+            lm_bff2d = np.mean(lm_bff2d, axis=1).reshape(-1,1)
+
+            lm_measurement = measure.Marker(lm_bff2d, idi)
+            measurements.append(lm_measurement)
+        
+        # Draw markers on image copy
+        img_marked = img.copy()
+        cv2.aruco.drawDetectedMarkers(img_marked, corners, ids)
+
+        return measurements, img_marked
